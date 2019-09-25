b171a35bb0baa51a8d719ea193ac49161264e696
Davide Libenzi <dlibenzi@google.com>
2019-09-24 18:27:37 -0700

    Multi-processing based multi-core TPU.

--- train.py
+++ train.py
@@ -10,8 +10,10 @@ import torch
 import torch.utils.data
 
 import torch_xla
-import torch_xla_py.xla_model as xm
-import torch_xla_py.data_parallel as dp
+import torch_xla.distributed.data_parallel as dp
+import torch_xla.utils.utils as xu
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.xla_multiprocessing as xmp
 
 from transformer import Transformer
 
@@ -35,25 +37,30 @@ class LazyDataset:
 
 def get_dataloader(path, batch_size, sequence_length, num_workers):
   dataset = LazyDataset(path, sequence_length + 1)
-  sampler = torch.utils.data.RandomSampler(dataset)
+  if xm.xrt_world_size() > 1:
+    sampler = torch.utils.data.distributed.DistributedSampler(
+        dataset,
+        num_replicas=xm.xrt_world_size(),
+        rank=xm.get_ordinal(),
+        shuffle=True)
+  else:
+    sampler = torch.utils.data.RandomSampler(dataset)
   return torch.utils.data.DataLoader(
       dataset, sampler=sampler, batch_size=batch_size, num_workers=num_workers)
 
 
-def main():
+def main(index):
   BATCH_SIZE = 128
   LOG_STEPS = 10
   METRICS_STEP = 50
   NUM_EPOCHS = 8
   SEQUENCE_LENGTH = 256
 
-  devices = xm.get_xla_supported_devices()
-  model = Transformer(256, 12, 512, 2048, 8, 0.2)
-  model_parallel = dp.DataParallel(model, device_ids=devices)
+  device = xm.xla_device()
+  model = Transformer(256, 12, 512, 2048, 8, 0.2).to(device)
+  optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 
-  def train_loop_fn(model, loader, device, context):
-    optimizer = context.getattr_or(
-        'optimizer', lambda: torch.optim.SGD(model.parameters(), lr=0.001))
+  def train_loop_fn(loader):
     tracker = xm.RateTracker()
 
     positions = torch.arange(SEQUENCE_LENGTH).long().view(
@@ -84,8 +91,9 @@ def main():
                                 BATCH_SIZE, SEQUENCE_LENGTH, 0)
 
   for epoch in range(0, NUM_EPOCHS):
-    model_parallel(train_loop_fn, train_loader)
+    para_loader = dp.ParallelLoader(train_loader, [device])
+    train_loop_fn(para_loader.per_device_loader(device))
 
 
 if __name__ == '__main__':
-  main()
+    xmp.spawn(main, args=())
